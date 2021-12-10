142a6df49486b4f3be3d8aef5f7b65647358896d
Amleshwar Pandey <vinesnimish@gmail.com>
2021-12-10 21:05:22 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,74 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harleys in Hawaii",
+    artist: "Katy Perry",
+    img: "harley-1",
+    src: "harley-1",
+  },
+  {
+    name: "STAY",
+    artist: "The Kid LAROI , Justin Bieber",
+    img: "stay-2",
+    src: "stay-2",
+  },
+  {
+    name: "The Search",
+    artist: "NF",
+    img: "search-3",
+    src: "search-3",
+  },
+  {
+    name: "Heat Waves",
+    artist: "Glass Animals",
+    img: "waves-4",
+    src: "waves-4",
+  },
+  {
+    name: "Dil luteya",
+    artist: "Sukhshinder | Romeo",
+    img: "luteya-5",
+    src: "luteya-5",
+  },
+  {
+    name: "Lovely",
+    artist: "Billie Eilish",
+    img: "lovely-6",
+    src: "lovely-6",
+  },
+  {
+    name: "Man Mera (Table no.21)",
+    artist: "Tina Desai &  Rajeev Khandelwal",
+    img: "manmera-7",
+    src: "manmera-7",
+  },
+  {
+    name: "Shape of You",
+    artist: "Ed Sheeran",
+    img: "shape-8",
+    src: "shape-8",
+  },
+  {
+    name: "Industry Baby",
+    artist: "Lil Nas X Jack Harlow",
+    img: "industry-9",
+    src: "industry-9",
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
